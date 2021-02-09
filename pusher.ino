7b33503e263822f3bc043e323da327b0378c593e
tvdv51093 <70633577+tvdv51093@users.noreply.github.com>
2021-02-09 17:26:00 +0100

    bijwerken

--- pusher.ino
+++ pusher.ino
@@ -1,101 +1,82 @@
 #include <Servo.h>
 
+/* Bedient een servo en duwt daarmee een blokje. */
+class duwer {
+	private:
+		Servo servo;
+		int pin = 0;
+	
+	public:
+		float rotatie; /* 0 tot 1.0f */
 
-
-/*#####################################################################
- * Block pusher.
- *###################################################################*/
-
-typedef struct pusher_t {
-	Servo servo;
-	uint32_t pin;
-	float rotation; /* 0 thru 1.0f */
-} pusher_t;
-
-/* Create new block pusher. */
-pusher_t create_pusher(uint32_t __pin)
-{
-	pusher_t pusher = {};
-	pusher.servo.attach(__pin);
-	pusher.pin = __pin;
-	pusher.rotation = 0.0f;
-
-	return pusher;
-}
-
-/* Write servo data to pin. */
-void update_pusher(pusher_t __pusher)
-{
-	/* Clamp to margin. */
-	const float margin = 0.03f;
-	if (__pusher.rotation > 1.0f - margin)
-		__pusher.rotation = 1.0f - margin;
-	else if (__pusher.rotation < margin)
-		__pusher.rotation = margin;
-
-	float angle = __pusher.rotation * 180.0f;
-	__pusher.servo.write((int)angle);
-}
-
-
-
-/*#####################################################################
- * Pusher Grid.
- *###################################################################*/
-
-/* Define grid of pushers. */
-#define GRID_WIDTH 1
-#define GRID_HEIGHT 1
-pusher_t grid[GRID_WIDTH][GRID_HEIGHT] = {};
-
-/* Create all pushers. */
-void create_grid(void)
-{
-	/* Encode grid pins. */
-	const uint32_t pusher_pins[GRID_WIDTH][GRID_HEIGHT] = {
-		{ 13 },
-	};
-
-	/* Create all pushers. */
-	for (uint32_t y = 0; y < GRID_HEIGHT; ++y) {
-		for (uint32_t x = 0; x < GRID_WIDTH; ++x) {
-			uint32_t pin = pusher_pins[x][y];
-			grid[x][y] = create_pusher(pin);
+		duwer() { }
+		duwer(int __pin) {
+			servo.attach(__pin);
+			pin = __pin;
+			rotatie = 0.0f;
 		}
-	}
-}
 
-void update_grid()
-{
-	/* Update all pushers. */
-	for (uint32_t y = 0; y < GRID_HEIGHT; ++y) {
-		for (uint32_t x = 0; x < GRID_WIDTH; ++x) {
-			pusher_t pusher = grid[x][y];
-			update_pusher(pusher);
+		void update() {
+			/* Hou binnen 0 en 1 */
+			float margin = 0.03f;		
+			if (rotatie > 1.0f - margin) 
+				rotatie = 1.0f - margin;
+			else if (rotatie < margin) 
+				rotatie = margin;
+
+			float hoek = rotatie * 180.0f;
+			servo.write((int)hoek);
 		}
-	}
-}
+};
 
+#define VELD_BREEDTE 2
+#define VELD_HOOGTE 2
 
+/* Bedient alle servo's. */
+class duwer_veld {
+	public:
+		duwer duwers[VELD_BREEDTE][VELD_HOOGTE];
 
-/*#####################################################################
- * Main.
- *###################################################################*/
+		duwer_veld() {
+			/* 2D veld van duwers */
+			const int pins[VELD_BREEDTE][VELD_HOOGTE] = {
+				{ 13, 12 },
+				{ 11, 10 }	
+			};
 
-/* Update pusher with grid[x][y].rotation = ... */
+			for (int y = 0; y < VELD_HOOGTE; ++y){
+				for (int x = 0; x < VELD_BREEDTE; ++x) {
+					duwers[x][y] = duwer(pins[x][y]);
+				}
+			}
+		}
+
+		void update() {
+			for (int y = 0; y < VELD_HOOGTE; ++y){
+				for (int x = 0; x < VELD_BREEDTE; ++x) {
+					duwers[x][y].update();
+				}
+			}
+		}	
+};
+
+duwer_veld *veld = nullptr;
 
 void setup() {
-	create_grid();
-
+	veld = new duwer_veld();
 }
+
+float time = 0.0f;
+int delayms = 20;
 
 void loop() {
-	delay(1000);
-	grid[0][0].rotation = 1.0f;
-	update_grid();
+	for (int y = 0; y < VELD_HOOGTE; ++y) {
+		for (int x = 0; x < VELD_BREEDTE; ++x){
+			veld->duwers[x][y].rotatie = 0.5f + 0.5f * sin((float)(x + y)/3.0f + time);
+		}
+	}
+	veld->update();
 
-	delay(1000);
-	grid[0][0].rotation = 0.0f;
-	update_grid();
+	time += delayms / 1000.0f;
+	delay(delayms);
 }
-
